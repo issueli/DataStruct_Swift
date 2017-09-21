7276adeccf9e99f280f7e2514877d8010af24ef4
issueli <issueli@163.com>
2017-09-22 00:41:47 +0800

    update tree property
    add height
    add node level

--- DataStruct/BinaryTree.swift
+++ DataStruct/BinaryTree.swift
@@ -8,23 +8,36 @@
 
 import Foundation
 
-var str = "Hello, playground"
-
 class Node {
     
     var data: Any
-    var leftChild: Node?
-    var rightChild: Node?
+    var level: Int = 0
+    var left: Node?
+    var right: Node?
     
     init(data: Any) {
         self.data = data
     }
+    
+    var isLeaf: Bool {
+        
+        get {
+            return (left == nil) && (right == nil)
+        }
+    }
+}
+
+enum NodeSide {
+    
+    case left
+    case right
 }
 
 class BinaryTree {
     
     var root: Node?
     var size = 0
+    var height = 0
     
     init() {}
     
@@ -33,20 +46,50 @@ class BinaryTree {
         self.removeLeft(node: nil)
     }
     
+    func insert(in node: Node?, side: NodeSide, data: Any) -> Bool {
+        
+        let newNode = Node(data: data)
+        
+        if node == nil {
+            
+            if size > 0 {
+                return false
+            }
+            root = newNode
+            height = 1
+            
+        } else {
+            
+            newNode.level = node!.level + 1
+            height = (height < newNode.level + 1) ? newNode.level + 1 : height
+            
+            switch side {
+            case .left:
+                node!.left = newNode
+            case .right:
+                node!.right = newNode
+            }
+        }
+        return true
+    }
+    
     func insertLeft(in node: Node?, data: Any) -> Bool {
         
         let newNode = Node(data: data)
-        newNode.data = data
         
         if node == nil {
             
             // 空树才可以设置为根节点
             if size > 0 { return false }
             root = newNode
-            
+            height = 1
         } else {
-            
-            node!.leftChild = newNode
+
+            newNode.level = node!.level + 1
+            if newNode.level + 1 > height {
+                height = newNode.level + 1
+            }
+            node!.left = newNode
         }
         
         size += 1
@@ -64,10 +107,15 @@ class BinaryTree {
             if size > 0 { return false }
             
             root = newNode
+            height = 1
             
         } else {
             
-            node!.rightChild = newNode
+            newNode.level = node!.level + 1
+            if newNode.level + 1 > height {
+                height = newNode.level + 1
+            }
+            node!.right = newNode
         }
         
         size += 1
@@ -80,7 +128,7 @@ class BinaryTree {
         
         if size == 0 { return }
         
-        var position = node == nil ? root : node!.leftChild
+        var position = node == nil ? root : node!.left
         
         guard let p = position else {
             return
@@ -98,7 +146,7 @@ class BinaryTree {
         
         if size == 0 { return }
         
-        var position = node == nil ? root : node!.rightChild
+        var position = node == nil ? root : node!.right
         
         guard let p = position else {
             return
@@ -116,32 +164,30 @@ class BinaryTree {
         
     }
     
-    func isEob(node: Node?) -> Bool {
+    func isEnd(node: Node?) -> Bool {
         
         return node == nil
     }
     
-    func isLeaf(node: Node) -> Bool {
+    static func height(tree: Node?) {
         
-        return ((node.leftChild == nil) && (node.rightChild == nil))
     }
     
     // 遍历二叉树
-    
     // 先序 根左右
     func preOrder(node: Node?) -> Array<Any> {
         
         var list = [Any]()
-        if !isEob(node: node) {
+        if !isEnd(node: node) {
             
             list.append(node!.data)
             
-            if !isEob(node: node?.leftChild) {
-                list.append(contentsOf: preOrder(node: node?.leftChild))
+            if !isEnd(node: node?.left) {
+                list.append(contentsOf: preOrder(node: node?.left))
             }
             
-            if !isEob(node: node?.rightChild) {
-                list.append(contentsOf: preOrder(node: node?.rightChild))
+            if !isEnd(node: node?.right) {
+                list.append(contentsOf: preOrder(node: node?.right))
             }
         }
         return list
@@ -152,16 +198,16 @@ class BinaryTree {
         
         var list = [Any]()
         
-        if !isEob(node: node) {
+        if !isEnd(node: node) {
             
-            if !isEob(node: node?.leftChild) {
-                list.append(contentsOf: inorder(node: node?.leftChild))
+            if !isEnd(node: node?.left) {
+                list.append(contentsOf: inorder(node: node?.left))
             }
             
             list.append(node!.data)
             
-            if !isEob(node: node?.rightChild) {
-                list.append(contentsOf: inorder(node: node?.rightChild))
+            if !isEnd(node: node?.right) {
+                list.append(contentsOf: inorder(node: node?.right))
             }
             
         }
@@ -172,14 +218,14 @@ class BinaryTree {
     func postorder(node: Node?) -> Array<Any> {
         
         var list = [Any]()
-        if !isEob(node: node) {
+        if !isEnd(node: node) {
             
-            if !isEob(node: node?.leftChild) {
-                list.append(contentsOf: postorder(node: node?.leftChild))
+            if !isEnd(node: node?.left) {
+                list.append(contentsOf: postorder(node: node?.left))
             }
             
-            if !isEob(node: node?.rightChild) {
-                list.append(contentsOf: postorder(node: node?.rightChild))
+            if !isEnd(node: node?.right) {
+                list.append(contentsOf: postorder(node: node?.right))
             }
             
             list.append(node!.data)
@@ -193,11 +239,11 @@ class BinaryTree {
         func s(left: Node?, right: Node?) -> Array<Any> {
             
             var list = [Any]()
-            if !isEob(node: left) {
+            if !isEnd(node: left) {
                 list.append(left!.data)
             }
             
-            if !isEob(node: right) {
+            if !isEnd(node: right) {
                 list.append(right!.data)
             }
             
@@ -206,10 +252,10 @@ class BinaryTree {
         
         var list = [Any]()
         
-        if !isEob(node: node) {
+        if !isEnd(node: node) {
             
             list.append(node!.data)
-            list.append(contentsOf: s(left: node?.leftChild, right: node?.rightChild))
+            list.append(contentsOf: s(left: node?.left, right: node?.right))
         }
         
         return list
